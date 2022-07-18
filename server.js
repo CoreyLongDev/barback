08e818f4adf72c47be92d3820d1d96a844b52a03
CoreylongDev <Coreylong90@icloud.com>
2022-07-18 10:41:34 -0400

    backend for Barcadia started. init backend API routes and simple server.js setup

---
+++ server.js
@@ -0,0 +1,40 @@
+require("dotenv").config()
+const express = require('express')
+const app = express()
+
+//LISTENER
+    const port = process.env.PORT || 3001
+    app.listen(port, () => {
+        console.log(`listening on port ${port}`)
+    })
+
+//BEER LIST
+    //frontend route - ./src/Pages/Reload.js
+    app.get('/api/v1/getBeers', (req, res) => {
+        console.log("get list of current beers items")
+        res.send("Beer List Here")
+    })
+    //no route yet
+    app.get('/api/v1/getBeers/:id', (req, res) => {
+        console.log(req.params)
+    })
+
+//EVENTS LIST
+    //frontend route - ./src/Pages/Events.js
+    app.get('/api/v1/getEvents', (req, res) => {
+        console.log("get list of current events items")
+        res.send("Calender Here")
+    })
+    app.get('/api/v1/getEvents/:id', (req, res) => {
+        console.log(req)
+    })
+
+//ARCADE LIST
+    //frontend route - ./src/Pages/Play.js
+    app.get('/api/v1/getGames', (req, res) => {
+        console.log("get list of current arcade floor items")
+        res.send("Games List Here")
+    })
+    app.get('/api/v1/getGames/:id', (req, res) => {
+        console.log(req)
+    })
