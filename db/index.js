250e9ad19991067949b0bb487848907298579331
CoreylongDev <Coreylong90@icloud.com>
2022-07-18 22:39:31 -0400

    SQL database connected ,tested, and pool protected via .env. mass routes per 3 tables good. now working on 3 tables via :id

--- db/index.js
+++ db/index.js
@@ -1,11 +1,5 @@
 const { Pool } = require('pg')
-const pool = new Pool({
-    user: 'coco',
-    host: 'localhost',
-    database: 'barcadia',
-    password: 'password',
-    port: 5400,
-})
+const pool = new Pool()
 module.exports = {
   query: (text, params) => pool.query(text, params),
 }
--- server.js
+++ server.js
@@ -5,43 +5,42 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(morgan("dev"));
-app.use((req, res, next) => {
-  console.log("middleware is gooood");
-  next();
-});
-//LISTENER
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);
-});
+app.use(express.json());
+
+
 
 //BEER LIST
 //frontend route - ./src/Pages/Reload.js
-app.get("/api/v1/getBeers", (req, res) => {
-  console.log("get list of current beers items");
-  res.send("Beer List Here");
+app.get("/api/v1/getBeers", async (req, res) => {
+    const results = await db.query("SELECT * FROM beers")
+    console.log(results)
 });
 //no route yet
 app.get("/api/v1/getBeers/:id", (req, res) => {
-  console.log(req.params);
+    console.log(req.params);
 });
 
 //EVENTS LIST
 //frontend route - ./src/Pages/Events.js
-app.get("/api/v1/getEvents", (req, res) => {
-  console.log("get list of current events items");
-  res.send("Calender Here");
+app.get("/api/v1/getEvents", async (req, res) => {
+    const results = await db.query("SELECT * FROM events")
+    console,log(results)
 });
 app.get("/api/v1/getEvents/:id", (req, res) => {
-  console.log(req);
+    console.log(req);
 });
 
 //ARCADE LIST
 //frontend route - ./src/Pages/Play.js
-app.get("/api/v1/getGames", (req, res) => {
-  console.log("get list of current arcade floor items");
-  res.send("Games List Here");
+app.get("/api/v1/getGames", async (req, res) => {
+    const results = await db.query("SELECT * FROM games")
 });
 app.get("/api/v1/getGames/:id", (req, res) => {
-  console.log(req);
+    console.log(req);
+});
+
+//LISTENER
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
 });
